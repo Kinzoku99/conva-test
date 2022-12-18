41efd6442eacf954043c65be8a3d1cb8f110f3ac
SzymonJakubicz <s.jakubicz@millionmonkeyssoftware.com>
2022-12-18 10:34:17 +0100

    draggable stage

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is conva test for ZPP
+// https://codesandbox.io/s/github/konvajs/site/tree/master/react-demos/dom_portal?from-embed
--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { Box } from "@chakra-ui/react"
+import { Box, ChakraProvider } from "@chakra-ui/react"
+import { Html } from 'react-konva-utils';
+import { Layer, Stage } from "react-konva";
 import { DummyComponent } from "./components";
 
-const App = () => (
-    <Box w="100wv" h="100vh" bg="gray.50">
-        <DummyComponent w="xl" />
-    </Box>
-);
+const App = () => {
+    //const box = new Rect
+    return (
+        <Box w="100vw" h="100vh" overflow="hidden" bg="gray.50">
+            <Stage width={window.innerWidth} height={window.innerHeight} draggable>
+                <Layer>
+                    <Html>
+                        <ChakraProvider>
+                            <DummyComponent w="xl" />
+                        </ChakraProvider>
+                    </Html>
+                </Layer>
+            </Stage>
+        </Box>
+    );
+}
 
 export default App;
